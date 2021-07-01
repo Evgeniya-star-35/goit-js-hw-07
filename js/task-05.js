ecb60b90bc75e31f1aaf184ba469520003af0692
Evgeniya Bayda <baje75128@gmail.com>
2021-07-01 12:06:13 +0300

    +

--- js/task-05.js
+++ js/task-05.js
@@ -1,21 +1,12 @@
-// const nameEl = document.querySelector('#name-input');
-// const nameOutput = document.querySelector('#name-output');
-// console.log(nameEl);
-// console.log(nameOutput);
-// nameEl.addEventListener('input', onFormInput);
-
 const refs = {
   nameInput: document.querySelector('#name-input'),
   nameOutput: document.querySelector('#name-output'),
 };
-refs.nameInput.addEventListener('input', onInputChange);
-function onInputChange(event) {
-  if (refs.nameInput.textContent === '') {
-    refs.nameInput.textContent = 'незнакомец';
+
+refs.nameInput.oninput = function (event) {
+  if (refs.nameInput.value === '') {
+    refs.nameOutput.innerHTML = 'незнакомец';
+  } else {
+    refs.nameOutput.innerHTML = refs.nameInput.value;
   }
-  refs.nameOutput.textContent = event.currentTarget.value;
-}
-// if (refs.nameInput.textContent === '') {
-//   refs.nameInput.textContent = 'незнакомец';
-// }
-// : refs.nameInput;
+};
