cc1397c7822f448691905896eb164a2e6a460fc3
Evgeniya Bayda <baje75128@gmail.com>
2021-07-08 06:05:03 +0300

    +

--- js/task-02.js
+++ js/task-02.js
@@ -8,14 +8,23 @@ const ingredients = [
   'Приправы',
 ];
 
-const ingredientsEl = ingredients.forEach(elem => {
-  const listElem = document.querySelector('#ingredients');
-  let itemElem = document.createElement('li');
+// const ingredientsEl = ingredients.forEach(elem => {
+//   const listElem = document.querySelector('#ingredients');
+//   let itemElem = document.createElement('li');
 
-  itemElem.append(elem);
-  // console.log(itemElem);
+//   itemElem.append(elem);
+//   // console.log(itemElem);
 
-  const fullIngredient = listElem.appendChild(itemElem);
-  console.log(fullIngredient);
-  return fullIngredient;
-});
+//   const fullIngredient = listElem.appendChild(itemElem);
+//   // console.log(fullIngredient);
+//   return fullIngredient;
+// });
+const listElem = document.querySelector('#ingredients');
+const itemGallery = document.createElement('li');
+console.log(itemGallery);
+
+const showGallery = ingredients.forEach(el =>
+  createGallery.insertAdjacentHTML('afterbegin', el),
+);
+
+listElem.insertAdjacentHTML('beforeend', showGallery);
